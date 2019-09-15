ed0fb414fd7e3596d15b87cd242a47f6313a7398
Sam BESSALAH <sam.bessalah@convexant.co.uk>
2019-09-16 00:14:22 +0200

    Clean connectors

--- connectors/register-connectors.sh
+++ connectors/register-connectors.sh
@@ -1,5 +1,3 @@
 #! /bin/bash
 
-#curl -i -X POST -H "Accept:application/json" -H  "Content-Type:application/json" http://localhost:8083/connectors/ -d @register-blocket-connector.json
-
-http POST http://localhost:8083/connectors < blocket-connector.json
+http POST http://localhost:8083/connectors < debezium-pg-connector.json
